dde24c55b45fb632c1920d7d2ddf44f7546527a8
Mike Monteith <mike@mikemonteith.com>
2021-03-10 18:07:35 +0000

    Add tests for the get_bound_block method and fix issues

--- streamfieldindex/models.py
+++ streamfieldindex/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from wagtail.core.blocks import ListBlock, StreamBlock, StructBlock
 
 
 class BlockTypes:
@@ -24,16 +25,30 @@ class IndexEntry(models.Model):
         field_value = getattr(self.page.specific, self.field_name)
         path = self.block_path.split("/")
 
-        iterator = iter(path)
-        block = field_value
-        for path_item in iterator:
-            if path_item.isdigit():
-                block = block[int(path_item)]
-                next(iterator)  # Iterate past the next item, it is the block's name which we don't need.
-            else:
-                block = block.value[path_item]
+        def get_sub_block(bound_block, path_list):
+            if len(path_list) == 0:
+                return bound_block
 
-        return block
+            path_item = path_list.pop(0)  # Pop the first element
+            if isinstance(bound_block.block, ListBlock):
+                index = int(path_item)
+                next_block = bound_block.block.child_block.bind(bound_block.value[index])
+                return get_sub_block(next_block, path_list)
+            elif isinstance(bound_block.block, StructBlock):
+                next_block = bound_block.value.bound_blocks[path_item]
+                return get_sub_block(next_block, path_list)
+            elif isinstance(bound_block.block, StreamBlock):
+                index = int(path_item)
+                path_list.pop(0)  # We can throw away the next path as it is just the block_type which we don't need
+                next_block = bound_block.value[index]
+                return get_sub_block(next_block, path_list)
+            else:
+                raise Exception(f"We don't know how to iterate over block type {type(bound_block.block)}")
+
+        first_index = path.pop(0)  # The first index must always be a number, since it is a streamfield
+        path.pop(0)  # We can throw away the next path as it is just the block_type which we don't need
+        block = field_value[int(first_index)]
+        return get_sub_block(block, path)
 
     def __str__(self):
         return f"<IndexEntry {self.page.title}::{self.field_name}::{self.block_path}>"
