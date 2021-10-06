05a206b9b9f17ff9576936f24151deb392bc8c04
Mike Monteith <mike@mikemonteith.com>
2021-10-06 10:00:46 +0100

    Index page after publish
    Pages can be published on a schedule, so indexing only when a page
    is created or edited is not enough.

--- streamfieldindex/wagtail_hooks.py
+++ streamfieldindex/wagtail_hooks.py
@@ -11,3 +11,8 @@ def index_after_create_page(request, page):
 @hooks.register("after_edit_page")
 def index_after_edit_page(request, page):
     index_page(page)
+
+
+@hooks.register("after_publish_page")
+def index_after_publish_page(page):
+    index_page(page)
